bc1124fcf640a687be66ed44ecbc29ab35a916b6
Byung-Hoon Kim <egyptdj@kaist.ac.kr>
2020-10-19 12:58:23 +0900

    update default example filename; update fmrinames

--- Examples/Scripts/IcaFixProcessingBatch.sh
+++ Examples/Scripts/IcaFixProcessingBatch.sh
@@ -2,7 +2,7 @@
 
 # Global default values
 DEFAULT_STUDY_FOLDER="${HOME}/data/Pipelines_ExampleData"
-DEFAULT_SUBJECT_LIST="100307"
+DEFAULT_SUBJECT_LIST="1001_01_MR"
 DEFAULT_ENVIRONMENT_SCRIPT="${HOME}/projects/Pipelines/Examples/Scripts/SetUpHCPPipeline.sh"
 DEFAULT_RUN_LOCAL="FALSE"
 #DEFAULT_FIXDIR="${HOME}/tools/fix1.06"  ##OPTIONAL: If not set will use $FSL_FIXDIR specified in EnvironmentScript
@@ -12,7 +12,7 @@ DEFAULT_RUN_LOCAL="FALSE"
 #	Get the command line options for this script
 #
 # Global Output Variables
-#	${StudyFolder}			- Path to folder containing all subjects data in subdirectories named 
+#	${StudyFolder}			- Path to folder containing all subjects data in subdirectories named
 #							  for the subject id
 #	${Subjlist}				- Space delimited list of subject IDs
 #	${EnvironmentScript}	- Script to source to setup pipeline environment
@@ -135,34 +135,35 @@ main() {
 	# set list of fMRI on which to run ICA+FIX, separate MR FIX groups with %, use spaces (or @ like dedrift...) to otherwise separate runs
 	# the MR FIX groups determine what gets concatenated before doing ICA
 	# the groups can be whatever you want, you can make a day 1 group and a day 2 group, or just concatenate everything, etc
-	fMRINames="tfMRI_WM_RL@tfMRI_WM_LR@tfMRI_GAMBLING_RL@tfMRI_GAMBLING_LR@tfMRI_MOTOR_RL@tfMRI_MOTOR_LR%tfMRI_LANGUAGE_RL@tfMRI_LANGUAGE_LR@tfMRI_SOCIAL_RL@tfMRI_SOCIAL_LR@tfMRI_RELATIONAL_RL@tfMRI_RELATIONAL_LR@tfMRI_EMOTION_RL@tfMRI_EMOTION_LR"
+	# fMRINames="tfMRI_WM_RL@tfMRI_WM_LR@tfMRI_GAMBLING_RL@tfMRI_GAMBLING_LR@tfMRI_MOTOR_RL@tfMRI_MOTOR_LR%tfMRI_LANGUAGE_RL@tfMRI_LANGUAGE_LR@tfMRI_SOCIAL_RL@tfMRI_SOCIAL_LR@tfMRI_RELATIONAL_RL@tfMRI_RELATIONAL_LR@tfMRI_EMOTION_RL@tfMRI_EMOTION_LR"
+	fMRINames="rfMRI_REST1_PA@rfMRI_REST1_AP@rfMRI_REST2_PA@rfMRI_REST2_AP"
 
 	# If you wish to run "multi-run" (concatenated) FIX, specify the names to give the concatenated output files
 	# In this case, all the runs included in ${fMRINames} become the input to multi-run FIX
 	# Otherwise, leave ConcatNames empty (in which case "single-run" FIX is executed serially on each run in ${fMRINames})
 	ConcatNames=""
-	ConcatNames="tfMRI_WM_GAMBLING_MOTOR_RL_LR@tfMRI_LANGUAGE_SOCIAL_RELATIONAL_EMOTION_RL_LR"  ## Use space (or @) to separate concatenation groups
+	# ConcatNames="tfMRI_WM_GAMBLING_MOTOR_RL_LR@tfMRI_LANGUAGE_SOCIAL_RELATIONAL_EMOTION_RL_LR"  ## Use space (or @) to separate concatenation groups
 
 	# set temporal highpass full-width (2*sigma) to use, in seconds, cannot be 0 for single-run FIX
-	bandpass=2000 
+	bandpass=2000
 	# MR FIX also supports 0 for a linear detrend, or "pdX" for a polynomial detrend of order X
 	# e.g., bandpass=pd1 is linear detrend (functionally equivalent to bandpass=0)
 	# bandpass=pd2 is a quadratic detrend
-	bandpass=0 #comment out for single run FIX and use above line for bandpass=2000
+	# bandpass=0 #comment out for single run FIX and use above line for bandpass=2000
 
 	# set whether or not to regress motion parameters (24 regressors)
 	# out of the data as part of FIX (TRUE or FALSE)
 	domot=FALSE
-	
+
 	# set training data file
 	TrainingData=HCP_hp2000.RData
 
 	# set FIX threshold (controls sensitivity/specificity tradeoff)
 	FixThreshold=10
-	
+
 	#delete highpass files (note that delete intermediates=TRUE is not recommended for MR+FIX)
 	DeleteIntermediates=FALSE
-	
+
 	# establish queue for job submission
 	QUEUE="-q hcp_priority.q"
 	if [ "${RunLocal}" == "TRUE" ]; then
@@ -175,13 +176,13 @@ main() {
 		echo ${Subject}
 
 		ResultsFolder="${StudyFolder}/${Subject}/MNINonLinear/Results"
-		
+
 		if [ -z "${ConcatNames}" ]; then
 			# single-run FIX
 			FixScript=${HCPPIPEDIR}/ICAFIX/hcp_fix
-			
+
 			fMRINamesFlat=$(echo ${fMRINames} | sed 's/[@%]/ /g')
-			
+
 			for fMRIName in ${fMRINamesFlat}; do
 				echo "  ${fMRIName}"
 
@@ -196,7 +197,7 @@ main() {
         	#need arrays to sanity check number of concat groups
         	IFS=' @' read -a concatarray <<< "${ConcatNames}"
         	IFS=% read -a fmriarray <<< "${fMRINames}"
-        	
+
         	if ((${#concatarray[@]} != ${#fmriarray[@]})); then
         	    echo "ERROR: number of names in ConcatNames does not match number of fMRINames groups"
         	    exit 1
@@ -236,4 +237,3 @@ main() {
 # Invoke the main function to get things started
 #
 main $@
-
